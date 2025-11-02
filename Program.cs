5383a45e8c71bef03198250ec3fb7d0eed2fefd6
Iryna <kytsenkoiryna33@gmail.com>
2025-11-02 21:59:49 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Delivery
+{
+    protected string type;
+    public Delivery(string type) { this.type = type; }
+    public virtual void Deliver() => Console.WriteLine($"Доставка: {type}");
+}
+
+class FastDelivery : Delivery
+{
+    public FastDelivery(string type) : base(type) { }
+    public override void Deliver() => Console.WriteLine($"dotnetШвидка доставка: {type}");
+}
+
+class Program
+{
+    static void Main()
+    {
+        Delivery flowers = new FastDelivery("Букет троянд ");
+        flowers.Deliver();
+    }
+}
+
+
+
+
+
+
+
+
